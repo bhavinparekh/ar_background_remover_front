cbc2d3b46dd359604f2e6bace4cec9231ff13289
Fatymata <fatymata.gassama@gmail.com>
2021-04-28 17:32:42 +0200

    [INIT] main page parts

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # AR_background_remover_front
 
 Front Interface for the background remover API
+
+## Install the dependencies
+
+```bash
+yarn install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
+```bash
+quasar dev
+```
--- webapp/src/pages/Index.vue
+++ webapp/src/pages/Index.vue
@@ -1,5 +1,19 @@
 <template>
-  <h2>Background remover interface</h2>
+  <div class="row justify-center items-center q-mt-xl">
+    <div class="col-10 text-center">
+      <p class="text-h6">Background remover interface</p>
+      <!------------------------------------- ADD A PICTURE ------------------------------------------>
+
+      <!------------------------------------------- END ---------------------------------------->
+
+      <!-- --------------------- MOBILE ONLY  (TAKE A PICTURE) ----------------------------------------->
+      <div v-if="$q.screen.lt.md">
+        Mobile
+      </div>
+
+      <!-- ---------------------  END MOBILE ONLY -------------------------------------->
+    </div>
+  </div>
 </template>
 
 <script>
